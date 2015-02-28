c9d02114f6954f8c0b3e0161c0968f27b3ccc196
steve b <meadsteve@gmail.com>
2015-02-28 17:09:21 +0000

    add logging

--- lib/white_bread/gherkin/parser.ex
+++ lib/white_bread/gherkin/parser.ex
@@ -1,20 +1,28 @@
 defmodule WhiteBread.Gherkin.Parser do
+  require Logger
   alias WhiteBread.Gherkin.Elements.Feature, as: Feature
 
   def parse_feature(feature_text) do
     {feature, _}  = feature_text
+    |> String.lstrip
     |> String.split("\n")
     |> Enum.reduce({%Feature{}, :start}, &process_line/2)
 
     feature
   end
 
-  defp process_line("Feature: " <> name, {feature, :start}) do
+  defp process_line("Feature: " <> name = line, {feature, :start}) do
+    log line
     {%{feature | name: String.rstrip(name)}, :start}
   end
 
-  defp process_line(_line, state) do
+  defp process_line(line, state) do
+    log line
     state
+  end
+
+  defp log(line) do
+    Logger.debug("Parsing line: #{line}")
   end
 
 end
