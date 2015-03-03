3001bd2f2d16f9a3ce0e3f707b4ba10e8a8dc71c
steve.brazier <steve.brazier@just-eat.com>
2015-03-03 15:52:44 +0000

    add test to ensure failing scenarios are returned

--- test/feature_runner_test.exs
+++ test/feature_runner_test.exs
@@ -20,6 +20,32 @@ defmodule WhiteBread.FeatureRunnerTest do
 
     assert result == %{failures: [], successes: [{scenario, {:ok, "test scenario"}}]}
   end
+
+  test "feature runner should return succesful and failed scenarios" do
+    steps = [
+      %Steps.When{text: "step one"},
+      %Steps.When{text: "step two"}
+    ]
+
+    failing_step = %Steps.When{text: "step two"}
+    failing_steps = [
+      %Steps.When{text: "step that blocks step two"},
+      failing_step
+    ]
+
+    scenario = %Scenario{name: "test scenario", steps: steps}
+    failing_scenario = %Scenario{name: "failing scenario", steps: failing_steps}
+    feature = %Feature{name: "test feature", scenarios: [scenario, failing_scenario]}
+
+    output = WhiteBread.Outputers.Console.start
+    result = WhiteBread.FeatureRunner.run(ExampleContext, feature, output)
+    output |> WhiteBread.Outputers.Console.stop
+
+    assert result == %{
+      failures: [{failing_scenario, {:failed, {:no_clause_match, failing_step}}}],
+      successes: [{scenario, {:ok, "test scenario"}}]
+    }
+  end
 end
 
 defmodule WhiteBread.FeatureRunnerTest.ExampleContext do
