d677e79655411d3110d658f758679bfc162fbcb7
steve.brazier <steve.brazier@just-eat.com>
2015-03-05 17:09:26 +0000

    add running white_bread example to travis

---
+++ lib/white_bread/example/example_context.ex
@@ -0,0 +1,45 @@
+defmodule WhiteBread.Example.ExampleContext do
+  use WhiteBread.Context
+
+  given_ ~r/there are (?<coffees>[0-9]+) coffees left in the machine/, fn _state, coffees: coffees ->
+    {:ok, %{} |> Dict.put(:coffees, coffees)}
+  end
+
+  given_ ~r/I have deposited £(?<pounds>[0-9]+)/, fn state, pounds: pounds ->
+    {:ok, state |> Dict.put(:pounds, pounds)}
+  end
+
+  when_ "I press the coffee button", fn
+    state = %{coffees: "1"} -> {:ok, state |> Dict.put(:coffees_served, 1)}
+    state = %{coffees: "0"} -> {:ok, state |> Dict.put(:coffees_served, 0)}
+  end
+
+  then_ "I should be served a coffee", fn state ->
+    served_coffees = state |> Dict.get(:coffees_served)
+    assert served_coffees == 1
+    {:ok, :whatever}
+  end
+
+  then_ "I should be frustrated", fn state ->
+    served_coffees = state |> Dict.get(:coffees_served)
+    assert served_coffees == 0
+    {:ok, :whatever}
+  end
+
+  given_ "I had a heart", fn _ ->
+    {:ok, "have a heart"}
+  end
+
+  given_ "I had a voice", fn _ ->
+    {:ok, "have a voice"}
+  end
+
+  then_ "I could love you", fn "have a heart" ->
+    {:ok, :love}
+  end
+
+  then_ "I would sing", fn "have a voice" ->
+    {:ok, :singing}
+  end
+
+end
