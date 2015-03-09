ef91ed6aa9eb3cf185f974cafcef597f7aa5fe4a
steve b <meadsteve@gmail.com>
2015-03-09 19:18:28 +0000

    handle feature sub directories

--- lib/white_bread/feature/finder.ex
+++ lib/white_bread/feature/finder.ex
@@ -1,15 +1,31 @@
 defmodule WhiteBread.Feature.Finder do
   import String, only: [ends_with?: 2]
+  import File, only: [dir?: 1]
 
   def find_in_path(dir_path) do
-    File.ls!(dir_path)
+    paths = File.ls!(dir_path)
+
+    root_files = paths
     |> get_only_feature_files
-    |> sort_alphabetically
     |> prepend_dir_path(dir_path)
+
+    sub_dir_files = paths
+    |> prepend_dir_path(dir_path)
+    |> get_only_directories
+    |> Enum.map(fn(sub_dir) -> find_in_path(sub_dir <> "/") end)
+    |> Enum.reduce([], fn(folder_files, all) -> all ++ folder_files end)
+
+    root_files
+    |> Enum.into(sub_dir_files)
+    |> sort_alphabetically
   end
 
   defp get_only_feature_files(file_paths) do
     file_paths |> Enum.filter(fn(file_path) -> file_path |> ends_with? ".feature" end)
+  end
+
+  defp get_only_directories(file_paths) do
+    file_paths |> Enum.filter(fn(file_path) -> file_path |> dir? end)
   end
 
   defp sort_alphabetically(file_paths) do
