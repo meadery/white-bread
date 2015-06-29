a61b005253bcae8f0645c731049fd223292f5e37
steve b <meadsteve@gmail.com>
2015-06-29 21:14:30 +0100

    add tests for parse/steps.ex

--- lib/white_bread/gherkin/parser/steps.ex
+++ lib/white_bread/gherkin/parser/steps.ex
@@ -1,12 +1,33 @@
 defmodule WhiteBread.Gherkin.Parser.Steps do
   alias WhiteBread.Gherkin.Elements.Steps, as: Steps
 
+  @doc ~S"""
+  Takes a string representing a step and adds it to the scenario as a struct
+
+  ## Examples
+
+  iex> add_step_to_scenario(%{steps: []}, "When I add this line")
+  %{steps: [%Steps.When{text: "I add this line"}]}
+
+  """
   def add_step_to_scenario(scenario, line) do
     step  = string_to_step(line)
     %{steps: current_steps} = scenario
     %{scenario | steps: current_steps ++ [step]}
   end
 
+  @doc ~S"""
+  Returns the appropriate struct for a step string
+
+  ## Examples
+
+  iex> string_to_step("Given this works")
+  %Steps.Given{text: "this works"}
+
+  iex> string_to_step("Then it might be useful")
+  %Steps.Then{text: "it might be useful"}
+
+  """
   def string_to_step(string) do
     case string do
       "Given " <> text -> %Steps.Given{text: text}
---
+++ test/gherkin/parser/steps_test.exs
@@ -0,0 +1,6 @@
+defmodule WhiteBread.Gherkin.Parser.StepsTest do
+  use ExUnit.Case
+  import WhiteBread.Gherkin.Parser.Steps
+  alias WhiteBread.Gherkin.Elements.Steps, as: Steps
+  doctest WhiteBread.Gherkin.Parser.Steps
+end
