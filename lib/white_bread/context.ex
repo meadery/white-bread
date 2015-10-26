7bcd9d33f8dd6fa92dfd80bb9a256659cb4eb296
steve.brazier <steve.brazier@just-eat.com>
2015-10-26 18:49:07 +0000

    consolidate step naming function

--- lib/white_bread/context.ex
+++ lib/white_bread/context.ex
@@ -102,7 +102,7 @@ defmodule WhiteBread.Context do
 
   # Regexes
   defp define_block_step({:sigil_r, _, _} = step_regex, block) do
-    function_name = regex_to_step_atom(step_regex)
+    function_name = step_name(step_regex)
     quote do
       @regex_steps [{unquote(step_regex), &__MODULE__.unquote(function_name)/2}
                     | @regex_steps]
@@ -114,7 +114,7 @@ defmodule WhiteBread.Context do
   end
 
   defp define_block_step(step_text, block) do
-    function_name = String.to_atom("step_" <> step_text)
+    function_name = step_name(step_text)
     quote do
       @string_steps @string_steps
         |> Dict.put(unquote(step_text), &__MODULE__.unquote(function_name)/2)
@@ -127,7 +127,7 @@ defmodule WhiteBread.Context do
 
   # regexes
   defp define_function_step({:sigil_r, _, _} = step_regex, function) do
-    function_name = regex_to_step_atom(step_regex)
+    function_name = step_name(step_regex)
     quote do
       @regex_steps [{unquote(step_regex), &__MODULE__.unquote(function_name)/2}
                     | @regex_steps]
@@ -141,7 +141,7 @@ defmodule WhiteBread.Context do
   end
 
   defp define_function_step(step_text, function) do
-    function_name = String.to_atom("step_" <> step_text)
+    function_name = step_name(step_text)
     quote do
       @string_steps @string_steps
        |> Dict.put(unquote(step_text), &__MODULE__.unquote(function_name)/2)
@@ -154,8 +154,11 @@ defmodule WhiteBread.Context do
     end
   end
 
-  defp regex_to_step_atom({:sigil_r, _, [{_, _, [string]}, _]}) do
+  defp step_name({:sigil_r, _, [{_, _, [string]}, _]}) do
     String.to_atom("regex_step_" <> string)
+  end
+  defp step_name(step_text) do
+    String.to_atom("step_" <> step_text)
   end
 
 end
