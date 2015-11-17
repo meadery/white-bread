d372fb9ffbee4973043c021a2512f49067e4b9ee
steve.brazier <steve.brazier@just-eat.com>
2015-11-17 11:32:11 +0000

    pull out common code in docstring parsing

--- lib/white_bread/gherkin/parser/helpers/doc_string.ex
+++ lib/white_bread/gherkin/parser/helpers/doc_string.ex
@@ -2,11 +2,9 @@ defmodule WhiteBread.Gherkin.Parser.GenericLine.Helpers.DocString do
 
   def add_doc_string_to_background_steps(line, feature, parser_state) do
     %{background_steps: current_steps} = feature
-    [%{doc_string: doc_string} = last_step | other_steps] = current_steps
-      |> Enum.reverse
 
-    updated_step = %{last_step | doc_string: doc_string <> line <> "\n"}
-    updated_steps = [updated_step | other_steps] |> Enum.reverse
+    updated_steps = current_steps
+      |> add_doc_string_to_last_step(line)
 
     {
       %{feature | background_steps: updated_steps},
@@ -17,16 +15,24 @@ defmodule WhiteBread.Gherkin.Parser.GenericLine.Helpers.DocString do
   def add_doc_string_to_step(line, feature, parser_state) do
     %{scenarios: [scenario | rest]} = feature
     %{steps: current_steps} = scenario
-    [%{doc_string: doc_string} = last_step | other_steps] = current_steps
-      |> Enum.reverse
 
-    updated_step = %{last_step | doc_string: doc_string <> line <> "\n"}
-    updated_steps = [updated_step | other_steps] |> Enum.reverse
+    updated_steps = current_steps
+      |> add_doc_string_to_last_step(line)
+
     updated_scenario = %{scenario | steps: updated_steps}
 
     {
       %{feature | scenarios: [updated_scenario | rest]},
       parser_state
     }
+  end
+
+  defp add_doc_string_to_last_step(current_steps, line) do
+    [%{doc_string: doc_string} = last_step | other_steps] = current_steps
+      |> Enum.reverse
+    updated_step = %{last_step | doc_string: doc_string <> line <> "\n"}
+    updated_steps = [updated_step | other_steps] |> Enum.reverse
+
+    updated_steps
   end
 end
