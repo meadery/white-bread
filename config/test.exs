94a3fd188926a9b15daaabc7ee5f221ed102756f
steve.brazier <steve.brazier@just-eat.com>
2015-11-17 14:48:31 +0000

    fix mistake in the dogma test exclusions

--- config/test.exs
+++ config/test.exs
@@ -4,4 +4,4 @@ config :logger, :console, level: :warn
 config :dogma,
   rule_set: Dogma.RuleSet.All,
   override: %{ ModuleDoc => false,
-               WindowsLineEndings => false}
+               ExceptionName => false}
