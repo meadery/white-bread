1894c6f22d7069dd45a473c2d327dd9384e4db13
steve.brazier <steve.brazier@just-eat.com>
2015-11-20 09:35:46 +0000

    tidy up scenario runner

--- lib/white_bread/runners/scenario_runner.ex
+++ lib/white_bread/runners/scenario_runner.ex
@@ -2,22 +2,22 @@ defimpl WhiteBread.Runners, for: WhiteBread.Gherkin.Elements.Scenario do
   alias WhiteBread.Outputers.ProgressReporter
 
   def run(scenario, context, setup) do
-    result = WhiteBread.Runners.run(
-      scenario.steps,
-      context,
-      setup
-    )
+    scenario.steps
+      |> WhiteBread.Runners.run(context, setup)
+      |> make_tuple(scenario)
+      |> output_result(setup.progress_reporter, scenario)
+  end
+
+  defp make_tuple(result, scenario) do
     case result do
       {:ok, _}   -> {:ok, scenario.name}
       error_data -> {:failed, error_data}
     end
-      |> output_result(setup.progress_reporter, scenario)
   end
 
   defp output_result(result_tuple, progress_reporter, scenario) do
     progress_reporter
       |> ProgressReporter.report({:scenario_result, result_tuple, scenario})
-
     result_tuple
   end
 end
