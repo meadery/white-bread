489b9064c78715392cd67dde02aecd9ee0068fbf
steve.brazier <meadsteve@gmail.com>
2015-11-21 11:46:46 +0000

    remove multiple log lines in parser

--- lib/white_bread/gherkin/parser/generic_line.ex
+++ lib/white_bread/gherkin/parser/generic_line.ex
@@ -6,107 +6,97 @@ defmodule WhiteBread.Gherkin.Parser.GenericLine do
   alias WhiteBread.Gherkin.Parser.Helpers.Tables, as: TableParser
   alias WhiteBread.Gherkin.Parser.Helpers.DocString, as: DocStringParser
 
-  def process_line("", state) do
-    Logger.debug("Parser skipping blank line")
+  def process_line(line, state) do
+    line
+      |> log
+      |> process(state)
+  end
+
+  defp process("", state) do
     state
   end
 
-  def process_line("#" <> _comment, state) do
+  defp process("#" <> _comment, state) do
     state
   end
 
-  def process_line("@" <> line, {feature, _state}) do
-    log line
+  defp process("@" <> line, {feature, _state}) do
     tags = line
       |> String.split("@", trim: true)
       |> Enum.map(&String.strip/1)
     {feature, %{tags: tags}}
   end
 
-  def process_line("Feature: " <> name = line, {feature, state}) do
-    log line
+  defp process("Feature: " <> name, {feature, state}) do
     feature_tags = tags_from_state(state)
     FeatureParser.start_processing_feature(feature, name, feature_tags)
   end
 
-  def process_line("Background:" <> _ = line, {feature, _} ) do
-    log line
+  defp process("Background:" <> _, {feature, _} ) do
     {feature, :background_steps}
   end
 
-  def process_line("Examples:" <> _ = line, {feature, _} ) do
-    log line
+  defp process("Examples:" <> _, {feature, _} ) do
     {feature, :scenario_outline_example}
   end
 
-  def process_line("Scenario: " <> name = line, {feature, state}) do
-    log line
+  defp process("Scenario: " <> name, {feature, state}) do
     tags = tags_from_state(state)
     ScenarioParser.start_processing_scenario(feature, name, tags)
   end
 
-  def process_line("Scenario Outline: " <> name = line, {feature, state}) do
-    log line
+  defp process("Scenario Outline: " <> name, {feature, state}) do
     tags = tags_from_state(state)
     ScenarioParser.start_processing_scenario_outline(feature, name, tags)
   end
 
   # Stop recoding doc string
-  def process_line(~s(""") <> _ = line, {feature, {:doc_string, prev_state}}) do
-    log line
+  defp process(~s(""") <> _, {feature, {:doc_string, prev_state}}) do
     DocStringParser.stop_processing_doc_string(feature, prev_state)
   end
 
   # Start recoding doc string
-  def process_line(~s(""") <> _ = line, {feature, state}) do
-    log line
+  defp process(~s(""") <> _, {feature, state}) do
     DocStringParser.start_processing_doc_string(feature, state)
   end
 
-  def process_line(line, {feature, {:doc_string, :background_steps} = state}) do
-    log line
+  defp process(line, {feature, {:doc_string, :background_steps} = state}) do
     DocStringParser.process_background_step_doc_string(line, feature, state)
   end
 
-  def process_line(line, {feature, { :doc_string, _prev_state } = state}) do
-    log line
+  defp process(line, {feature, { :doc_string, _prev_state } = state}) do
     DocStringParser.process_scenario_step_doc_string(line, feature, state)
   end
 
   # Tables as part of a step
-  def process_line("|" <> line, {feature, :scenario_steps}) do
-    log line
+  defp process("|" <> line, {feature, :scenario_steps}) do
     TableParser.process_step_table_line(line, feature)
   end
 
   # Tables as part of an example for a scenario
-  def process_line("|" <> line, {feature, :scenario_outline_example}) do
-    log line
+  defp process("|" <> line, {feature, :scenario_outline_example}) do
     TableParser.process_outline_table_line(line, feature)
   end
 
-  def process_line(line, {feature, :feature_description}) do
-    log line
+  defp process(line, {feature, :feature_description}) do
     FeatureParser.process_feature_desc_line(line, feature)
   end
 
-  def process_line(line, {feature, :background_steps}) do
-    log line
+  defp process(line, {feature, :background_steps}) do
     StepsParser.process_background_step_line(line, feature)
   end
 
-  def process_line(line, {feature, :scenario_steps}) do
-    log line
+  defp process(line, {feature, :scenario_steps}) do
     StepsParser.process_scenario_step_line(line, feature)
   end
 
-  def process_line(line, state) do
-    log line
+  defp process(_line, state) do
     state
   end
 
   defp log(line) do
     Logger.debug(~s(Parsing line: "#{line}"))
+    line
   end
 
   defp tags_from_state(state) do
