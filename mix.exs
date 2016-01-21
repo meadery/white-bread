96befd6fed5bcb9774f29f74903fe1dfcd461624
stephen.brazier <stephen.brazier@crowdcube.com>
2016-01-21 08:32:39 +0000

    bump mix version

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule WhiteBread.Mixfile do
        licenses: ["MIT"],
        links: %{"GitHub" => "https://github.com/meadsteve/white-bread"},
        ],
-     version: "2.2.0",
+     version: "2.3.0",
      deps: deps]
   end
 
