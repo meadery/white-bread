813ee82b0f739deefa01368cc0f8b2698cae38fc
Richard Kotze <rkotze@dctfh.com>
2016-02-01 22:30:38 +0000

    use context_per_feature module decide if its on and build suites based on config.

--- lib/white_bread/command_line/suite_run.ex
+++ lib/white_bread/command_line/suite_run.ex
@@ -1,7 +1,7 @@
 defmodule WhiteBread.CommandLine.SuiteRun do
   alias WhiteBread.CommandLine.ContextLoader
+  alias WhiteBread.CommandLine.ContextPerFeature
   alias WhiteBread.Suite
-  alias WhiteBread.ContextFeature
 
   def run_suites(
     _options,
@@ -9,31 +9,17 @@ defmodule WhiteBread.CommandLine.SuiteRun do
     config_path: config_path,
     contexts: context_path)
   do
-    handle_runner = fn
-      {:per_feature, contexts} -> context_per_feature_results(contexts)
-      {:suites, suites} -> suite_results(suites)
+    handle_suites = fn
+      {:ok, context_feature_suites}, suites -> [context_feature_suites | suites]
+      {:error, _}, suites -> suites
     end
 
     ContextLoader.load_context_files(context_path)
 
-    IO.puts "****** Has module *****"
-    IO.inspect Code.ensure_loaded?(Module.concat(["WhiteBread", "Example", DefaultContext]))
-    # get all feature file names
-    # make list of modules out of it using Module.concat
-    # Make a feature struct and pass to new run_suite for features
-
-    handle_runner.(get_suites_from_config(config_path))
-  end
-
-  defp context_per_feature_results(context_features) do
-    context_features
-      |> Enum.map(&run_context_per_feature/1)
-      |> Enum.flat_map(fn results -> results.failures end)
-  end
-
-  defp run_context_per_feature(%ContextFeature{} = context_feature) do
-    IO.puts "\n\n**** Context per feature"
-    WhiteBread.run(context_feature.context, context_feature.feature_path, tags: [])
+    {context_features, suites} = get_suites_from_config(config_path)
+    
+    handle_suites.(ContextPerFeature.build_suites(context_features), suites) |>
+    suite_results
   end
 
   defp suite_results(suites) do
@@ -51,12 +37,7 @@ defmodule WhiteBread.CommandLine.SuiteRun do
   defp get_suites_from_config(path) do
     IO.puts "loading config from #{path}"
     [{suite_config_module, _} | _] = Code.load_file(path)
-    case suite_config_module.context_per_feature do
-      {:on, true} ->
-        {:per_feature, suite_config_module.context_per_feature}
-      _ ->
-        {:suites, suite_config_module.suites }
-    end
+    {suite_config_module.context_per_feature, suite_config_module.suites}
   end
 
 end
