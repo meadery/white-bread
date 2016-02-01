98eb9ed2a7344e1cb50c82744f67d7a035cd8324
Richard Kotze <rkotze@dctfh.com>
2016-02-01 23:30:05 +0000

    Add fake acceptance test in.

---
+++ features/contexts/my_feature_a_context.exs
@@ -0,0 +1,8 @@
+defmodule WhiteBread.TestContextPerFeature.MyFeatureAContext do
+  use WhiteBread.Context
+
+  given_ ~r/^(?<anything>.+)$/, fn state, %{anything: _anything} ->
+    {:ok, state}
+  end
+  
+end
---
+++ features/test_context_per_feature/my_feature_a.feature
@@ -0,0 +1,6 @@
+Feature: Toast my bread
+
+  Scenario: The desire to have toasted bread
+    Given I have bread
+    When I place in the toaster
+    Then I should get toast
