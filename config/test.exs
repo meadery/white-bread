4812695ffb6773e2c19fe20bac8fb75da64d81a5
steve.brazier <meadsteve@gmail.com>
2016-09-29 14:03:27 +0100

    remove dogm and bump deps

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,2 @@
 use Mix.Config
 config :logger, :console, level: :warn
-
-config :dogma,
-  rule_set: Dogma.RuleSet.All,
-  override: %{ ModuleDoc => false,
-               ExceptionName => false}
--- lib/white_bread/runners/scenario_outline_runner.ex
+++ lib/white_bread/runners/scenario_outline_runner.ex
@@ -13,7 +13,9 @@ defmodule WhiteBread.Runners.ScenarioOutlineRunner do
   end
 
   defp process_results([], _), do: [{:failed, :no_examples_given}]
-  defp process_results(results, scenario_outline), do: Enum.map(results, &process_result(&1, scenario_outline))
+  defp process_results(results, scenario_outline) do
+    Enum.map(results, &process_result(&1, scenario_outline))
+  end
 
   defp process_result({:ok, _last_state}, scenario), do: {:ok, scenario.name}
   defp process_result(error_data,        _scenario), do: {:failed, error_data}
