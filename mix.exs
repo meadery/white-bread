501f092325818379fc55bb1e0726597b8e39ab95
steve.brazier <meadsteve@gmail.com>
2016-09-29 14:15:00 +0100

    mix version to 2.8,1

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule WhiteBread.Mixfile do
        licenses: ["MIT"],
        links: %{"GitHub" => "https://github.com/meadsteve/white-bread"},
        ],
-     version: "2.8.0",
+     version: "2.8.1",
      aliases: aliases,
      deps: deps]
   end
