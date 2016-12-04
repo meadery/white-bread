a26b9da37878a71f3b64076b3c73830d17fdb961
Marnen Laibow-Koser <marnen@marnen.org>
2016-12-03 23:14:32 -0800

    Reorganize directories so white_bread doesn't get confused about which context to use. [#73]

--- test/feature/finder_test.exs
+++ test/feature/finder_test.exs
@@ -2,13 +2,13 @@ defmodule WhiteBread.Feature.FinderTest do
   use ExUnit.Case
   alias WhiteBread.Feature.Finder
 
-  test "finds only the six feature files" do
-    result = Finder.find_in_path("features/")
-    assert Enum.count(result) == 8
+  test "finds only the five feature files" do
+    result = Finder.find_in_path("features/default/")
+    assert Enum.count(result) == 7
   end
 
   test "accepts a list of paths" do
-    result = Finder.find_in_path(["features/sub_folder/", "features/sub_folder_two/"])
+    result = Finder.find_in_path(["features/default/sub_folder/", "features/default/sub_folder_two/"])
     assert Enum.count(result) == 2
   end
 
