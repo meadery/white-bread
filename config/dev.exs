0ca9a1905ca0108285b7c64b86a863d46daaa241
Joseph Yiasemides <joseph.yiasemides@erlang-solutions.com>
2017-01-23 17:00:13 +0100

    Configure both Console and HTML outputers under the test environment

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,2 @@
 use Mix.Config
 config :logger, :console, level: :warn
-
-config :white_bread,
-  outputers: [{WhiteBread.Outputers.Console, []},
-              {WhiteBread.Outputers.HTML, path: "~/fu/bar/baz.html"}
-             ]
--- config/test.exs
+++ config/test.exs
@@ -1,2 +1,7 @@
 use Mix.Config
 config :logger, :console, level: :warn
+
+config :white_bread,
+  outputers: [{WhiteBread.Outputers.Console, []},
+              {WhiteBread.Outputers.HTML, path: "~/fu/bar/baz.html"}
+             ]
