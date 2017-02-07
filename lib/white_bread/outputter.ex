8dcced1697659e0eb761fae80e44e18b5ae53036
steve.brazier <meadsteve@gmail.com>
2017-02-07 12:02:42 +0000

    Limit the kind of data structure that can be reported

--- lib/white_bread/outputter.ex
+++ lib/white_bread/outputter.ex
@@ -1,7 +1,16 @@
 defmodule WhiteBread.Outputter do
+  alias WhiteBread.EventManager
 
-  def report(thing) do
-    WhiteBread.EventManager.report(thing)
+  def report({:final_results, result_map}) do
+    EventManager.report({:final_results, result_map})
+  end
+
+  def report({:suite, suite.name}) do
+    EventManager.report({:suite, suite.name})
+  end
+
+  def report({:scenario_result, result_tuple, scenario_or_outline}) do
+    EventManager.report({:scenario_result, result_tuple, scenario_or_outline})
   end
 
 end
