abe4a9e3dd6495212c0d25e924ab3f043a0129cf
Leif Gensert <leif@leif.io>
2017-09-06 15:48:47 +0200

    upgrade dependencies

--- mix.exs
+++ mix.exs
@@ -55,11 +55,11 @@ defmodule WhiteBread.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:gherkin, "~> 1.0"},
+      {:gherkin, "< 1.2.0"},
 
-      {:credo, "~> 0.5", only: :dev},
-      {:earmark, "~> 1.0.1", only: :dev},
-      {:ex_doc, "~> 0.8", only: :dev}
+      {:credo, "~> 0.8", only: :dev},
+      {:earmark, "~> 1.2", only: :dev},
+      {:ex_doc, "~> 0.16", only: :dev}
     ]
   end
 
